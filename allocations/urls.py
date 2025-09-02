56935678d2e57a7b7529a7482df4c55803c86bdd
Musharaka User <user@example.com>
2025-09-02 12:51:26 +0000

    Update allocations URLs to remove detail view and rename list view
    Co-authored-by: mohagag609 <mohagag609@gmail.com>

--- allocations/urls.py
+++ allocations/urls.py
@@ -4,7 +4,6 @@ from . import views
 app_name = 'allocations'
 
 urlpatterns = [
-    path('<int:project_id>/', views.allocations_list, name='list'),
+    path('<int:project_id>/', views.allocations_history, name='list'),
     path('<int:project_id>/stage/<int:stage_id>/', views.allocate_delta, name='allocate'),
-    path('<int:project_id>/allocation/<int:allocation_id>/', views.allocation_detail, name='detail'),
 ]
